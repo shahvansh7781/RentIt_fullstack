3d63d25fcf4edfd0f63373c5e36ad2aca46182a1
gautam <gautammaiyani2332@gmai.com>
2022-10-31 13:46:56 +0530

    Gautam Patel : New Component-> Login && add related dependeces && some change in App.js && Public-> index.js -> adding related link

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,46 @@ import Navbar from "./Components/Navbar/Navbar";
 import Landing from "./Components/LandingPage/Landing";
 import Home from "./Components/Home/Home";
 import Cards from "./Components/Cars/Cards";
+import { MainContainer } from "./Components/Login/MainContainer";
+import {useState} from 'react'
+
+
 function App() {
+
+
+  const[container,setContainer]=useState('logined');
+  const[logoClass,setLogoClass]=useState('logo blue');
+  const changeContainerState=()=>{
+
+    if(container==='logined'){
+
+       setContainer('registered');
+       setLogoClass('logo');
+    }
+    else if(container==='registered'){
+      setContainer('logined')
+      setLogoClass('logo blue');
+    }
+
+  }
   return (
     <>
       {/* <Navbar/> */}
 
-      <Routes>
+       <Routes>
+
+      
         <Route exact path="/" element={<Landing />}></Route>
         <Route exact path="/home" element={<Home />}></Route>
         <Route exact path="/cars" element={<Cards/>}></Route>
-      </Routes>
+        <Route exact path="/login" element={<MainContainer container={container}
+          changeContainerState={changeContainerState}
+          logoClass={logoClass} />}>
+        </Route>
+
+      </Routes> 
+
+
     </>
   );
 }
