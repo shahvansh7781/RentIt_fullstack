75480d23a53ea63f564291933ee852cca581e965
shahvansh7781 <vanshshah880@gmail.com>
2022-10-31 12:53:51 +0530

    Vansh: Changes in Landing and Home Page

--- frontend/src/Components/LandingPage/Landing.jsx
+++ frontend/src/Components/LandingPage/Landing.jsx
@@ -9,7 +9,7 @@ const Landing = () => {
       <Navbar/>
       <div className="main-content">
         <div className="inner-content">
-            <h1>Welcome to RentIT</h1>
+            <h1>Welcome to <span style={{color:"#03a8b0"}}> RentIT </span></h1>
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. <br /> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,<br /> when an unknown printer  took a galley  of type  and scrambled it to make a type <br /> specimen book. It has survived not only five centuries, but also the leap into <br /> electronic typesetting, remaining essentially unchanged. <br /> Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quam ipsa velit culpa, <br /> vero soluta voluptatum voluptatibus doloribus, laudantium assumenda deserunt <br /> vel qui nisi necessitatibus nulla nobis voluptate mollitia unde.</p>
             <Link className="mylink1" to="/home">Get Started</Link>
         </div>
