21930b05fe30e100e2fc93a986a82e75c9f1e3f8
shahvansh7781 <vanshshah880@gmail.com>
2022-12-24 14:56:21 +0530

    about us changes

--- frontend/src/Components/AboutUs/About.jsx
+++ frontend/src/Components/AboutUs/About.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import Testimonials from './Testimonials/Testimonials'
-import './About.css'
-import { motion } from 'framer-motion'
+import React from "react";
+import Testimonials from "./Testimonials/Testimonials";
+import "./About.css";
+import { motion } from "framer-motion";
 // import Footer from '../Footer/Footer'
-import Counters from './Counters/Counters'
-import Navbar from '../Navbar/Navbar'
-import Footer from '../Footer/Footer'
-import Background from './bg.webp'
-import { Link } from 'react-router-dom'
+import Counters from "./Counters/Counters";
+import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
+import Background from "./bg.webp";
+import { Link } from "react-router-dom";
 // import Counters from '../Counters/Counters'
 
 const textAnimation = {
@@ -19,9 +19,9 @@ const textAnimation = {
     y: 0,
     x: 0,
     opacity: 1,
-    transition: { type: 'spring', bounce: 0.4, duration: 6 },
+    transition: { type: "spring", bounce: 0.4, duration: 6 },
   },
-}
+};
 const pagesAnimation = {
   hidden: {
     opacity: 0,
@@ -31,100 +31,115 @@ const pagesAnimation = {
   visible: {
     opacity: 1,
     x: 0,
-    transition: { delay: 0.15, duration: 2.5, type: 'spring' },
+    transition: { delay: 0.15, duration: 2.5, type: "spring" },
   },
-}
-
-
+};
 
 const About = () => {
-  const transition = { type: 'spring', duration: 3 }
+  const transition = { type: "spring", duration: 3 };
   return (
     <motion.div
       variants={pagesAnimation}
-      initial='hidden'
-      animate='visible'
-      className='aboutUs'
+      initial="hidden"
+      animate="visible"
+      className="aboutUs"
     >
-      <div className='parallax'>
+      <div className="parallax">
         {/* style={{ backgroundImage: `url(${Background}) norepeat` }} */}
         <div
           variants={pagesAnimation}
-          initial='hidden'
-          animate='visible'
-          className='bg'
+          initial="hidden"
+          animate="visible"
+          className="bg"
         >
           <Navbar />
           <header>
             <motion.div
               variants={textAnimation}
-              initial='hidden'
-              animate='visible'
-              className='text'
+              initial="hidden"
+              animate="visible"
+              className="text"
             >
-              <h1 style={{ color: 'white' }}>RentIt</h1>
-              <h1 style={{ color: 'white' }}>About Us</h1>
+              <h1 style={{ color: "white" }}>RentIt</h1>
+              <h1 style={{ color: "white" }}>About Us</h1>
             </motion.div>
           </header>
         </div>
       </div>
-      <Counters></Counters>
+      <Counters />
 
-      <div className='main'>
-        <div className='partition'>
-          <div className='leftp'></div>
-          <div className='centerp'>
-            <motion.img
-              src='https://images.pexels.com/photos/305070/pexels-photo-305070.jpeg?auto=compress&cs=tinysrgb&w=600'
+      <div className="main">
+        <div className="partition">
+          <div className="leftp">
+            <div style={{marginLeft:"17vmax",display:"flex",justifyContent:"center",alignItems:"center"}}>
+              <motion.img
+                src="https://images.pexels.com/photos/305070/pexels-photo-305070.jpeg?auto=compress&cs=tinysrgb&w=600"
+                initial={{ opacity: 0, x: -100 }}
+                transition={{ ...transition, duration: 2 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                style={{
+                  height: "30vmax",
+                  width: "27vmax",
+                  padding:"3vmax",
+                  position:"relative",
+                  zIndex:9999,
+                }}
+              ></motion.img>
+            </div>
+          </div>
+          <div>
+            {/* <motion.img
+              src="https://images.pexels.com/photos/305070/pexels-photo-305070.jpeg?auto=compress&cs=tinysrgb&w=600"
               initial={{ opacity: 0, x: -100 }}
               transition={{ ...transition, duration: 2 }}
               whileInView={{ opacity: 1, x: 0 }}
-            ></motion.img>
+            ></motion.img> */}
           </div>
-          <div className='rightp'>
-            <motion.p
+          <div className="rightp">
+            {/* <motion.p
               initial={{ opacity: 0, y: 100 }}
               transition={{ ...transition, duration: 2 }}
               whileInView={{ opacity: 1, y: 0 }}
-            >
-              <motion.span>About Us</motion.span>
-              <br></br>
-              <br></br>
-              <motion.span className='head'>Welcome To RentIt</motion.span>
-              <br></br>
-              <br></br>A small river named Duden flows by their place and
-              supplies it with the necessary regelialia. It is a paradisematic
-              country, in which roasted parts of sentences fly into your mouth.
-              On her way she met a copy. The copy warned the Little Blind Text,
-              that where it came from it would have been rewritten a thousand
-              times and everything that was left from its origin would be the
-              word "and" and the Little Blind Text should turn around and return
-              to its own, safe country. A small river named Duden flows by their
-              place and supplies it with the necessary regelialia. It is a
+            > */}
+            {/* <motion.span> */}
+            <div style={{ fontSize: "2.5vmax",marginLeft:"8vmax" }}> About Us</div>
+            {/* </motion.span> */}
+
+            {/* <motion.span className="head"> */}
+            <div style={{ fontSize: "1.7vmax",marginLeft:"8vmax" }}> Welcome To RentIt</div>
+            {/* </motion.span> */}
+            <div style={{ fontSize: "1vmax",marginLeft:"8vmax" }}>
+              A small river named Duden flows by their place and supplies it
+              with the necessary regelialia. It is a paradisematic country, in
+              which roasted parts of sentences fly into your mouth. On her way
+              she met a copy. The copy warned the Little Blind Text, that where
+              it came from it would have been rewritten a thousand times and
+              everything that was left from its origin would be the word "and"
+              and the Little Blind Text should turn around and return to its
+              own, safe country. A small river named Duden flows by their place
+              and supplies it with the necessary regelialia. It is a
               paradisematic country, in which roasted parts of sentences fly
-              into your mouth.<br></br>
-              <Link to='/cars'>
-                <button>Book A Car</button>
+              into your mouth.
+            </div>
+            <div style={{ marginTop: "16px",marginLeft:"8vmax" }}>
+              <Link to="/cars" className="carousel-button">
+                Book A Car
               </Link>
-            </motion.p>
+            </div>
+            {/* </motion.p> */}
           </div>
         </div>
 
-        <div className='image'></div>
+        {/* <div className="image"></div> */}
       </div>
-      <br></br>
-      <br></br>
-      <Testimonials></Testimonials>
-      <br></br>
-      <br></br>
 
-      <br></br>
-      <br></br>
+      <Testimonials />
+
       <hr></hr>
 
       <Footer />
     </motion.div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- frontend/src/Components/AboutUs/Testimonials/Testimonials.jsx
+++ frontend/src/Components/AboutUs/Testimonials/Testimonials.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import './Testimonials.css'
+import React from "react";
+import "./Testimonials.css";
 // import { testimonialData } from 'data'
-import { useState } from 'react'
+import { useState } from "react";
 
-import { motion } from 'framer-motion'
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos'
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
-import { testimonialData } from './data'
+import { motion } from "framer-motion";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import { testimonialData } from "./data";
 
 const Testimonials = () => {
-  const transition = { type: 'spring', duration: 3 }
-  const [selected, setSelected] = useState(0)
+  const transition = { type: "spring", duration: 3 };
+  const [selected, setSelected] = useState(0);
   const tLength = testimonialData.length;
   return (
-    <div className='testimonials'>
-      <div className='left-t'>
+    <div className="testimonials">
+      <div className="left-t">
         <span>Testimonials</span>
-        <span className='stroke-text'>What they say</span>
+        <span className="stroke-text">What they say</span>
         <span>about us</span>
         <motion.span
           key={selected}
@@ -31,12 +31,12 @@ const Testimonials = () => {
           <span>{testimonialData[selected].name}</span>
 
           <span>
-            {'  -'}
+            {"  -"}
             {testimonialData[selected].status}
           </span>
         </span>
       </div>
-      <div className='right-t'>
+      <div className="right-t">
         <motion.div
           initial={{ opacity: 0, x: -100 }}
           transition={{ ...transition, duration: 2 }}
@@ -60,30 +60,30 @@ const Testimonials = () => {
           transition={{ ...transition, duration: 2 }}
           whileInView={{ opacity: 1, x: 0 }}
           src={testimonialData[selected].image}
-          alt=''
+          alt=""
         />
 
-        <div className='arrows'>
+        <div className="arrows">
           <ArrowBackIosIcon
             onClick={() => {
               selected === 0
                 ? setSelected(tLength - 1)
-                : setSelected((prev) => prev - 1)
+                : setSelected((prev) => prev - 1);
             }}
-            style={{ color: '#3591ca', height: '1.7rem' }}
+            style={{ color: "#3591ca", height: "1.7rem" }}
           />
           <ArrowForwardIosIcon
             onClick={() => {
               selected === tLength - 1
                 ? setSelected(0)
-                : setSelected((prev) => prev + 1)
+                : setSelected((prev) => prev + 1);
             }}
-            style={{ color: '#3591ca', height: '1.7rem' }}
+            style={{ color: "#3591ca", height: "1.7rem" }}
           />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Testimonials;
